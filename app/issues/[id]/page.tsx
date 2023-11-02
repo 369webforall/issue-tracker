18a5d8d18d51dc3ffaa1dd7e22498b0d82c7afe0
369webforall <369webforall@gmail.com>
2023-11-02 17:37:02 +0545

    Apply:SRP

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -6,6 +6,8 @@ import IssueStatusBadge from '../../components/IssueStatusBadge';
 import ReactMarkdown from 'react-markdown';
 import { Pencil2Icon } from '@radix-ui/react-icons';
 import Link from 'next/link';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 interface Props {
   params: { id: string };
 }
@@ -18,20 +20,10 @@ const IssueDetailsPage = async ({ params }: Props) => {
   return (
     <Grid columns={{ initial: '1', md: '2' }} gap="4">
       <Box>
-        <Heading>{issue.title}</Heading>
-        <Flex gap="4" my="4">
-          <IssueStatusBadge status={issue.status} />
-          <p>{issue.createdAt.toDateString()}</p>
-        </Flex>
-        <Card className="prose" my="4">
-          <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
+        <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/edit/${issue.id}`}>Update Issue</Link>
-        </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   );
