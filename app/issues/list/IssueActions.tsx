ff766918e3777599db937550d2d46c8be3d86c7c
369webforall <369webforall@gmail.com>
2023-11-13 14:21:43 +0545

    Building the Filter Component

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Button } from '@radix-ui/themes';
+import { Button, Flex } from '@radix-ui/themes';
 import Link from 'next/link';
+import IssueStatusFilter from './IssueStatusFilter';
 const IssueActions = () => {
   return (
-    <div className="mb-4">
+    <Flex mb="4" justify="between">
+      <IssueStatusFilter />
       <Button>
         <Link href="/issues/new">Add New Issue</Link>
       </Button>
-    </div>
+    </Flex>
   );
 };
 
